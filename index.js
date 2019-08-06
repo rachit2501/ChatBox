e01d8d36093de4b0d8e578fc026cabcf592e748d
rachit2501 <rac.sri25@gmail.com>
2019-08-06 11:29:52 +0530

    bug fixing left

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'),
 app = express(),
 server = require('http').createServer(app),
 io=require('socket.io').listen(server);
-
+username = [];
 server.listen(process.env.PORT || 3000);
 console.log('Server Running....');
 
@@ -13,9 +13,37 @@ app.get('/',function(req , res)
 
 io.sockets.on('connection' , function(socket){
 	console.log('Socket Connected.....');
+	socket.on('new user' , async function(data)
+	{
+		let result = await username.indexOf(data);
+		if(!result)
+		{
+			console.log("already a user");
+		}    
+		else
+		{
+			socket.username = data;
+			username.push(socket.username);
+			updateUsernames();
+		}
+    });
 
+    function updateUsernames()
+    {
+    	io.emit('usernames' , username);
+    }
 	socket.on('send message', function(data)
 	{
-		io.sockets.emit('new message',{msg : data});
+		io.sockets.emit('new message',{msg : data , user: socket.user});
+	});
+
+	socket.on('disconnect', function(data)
+	{
+		if(!socket.username){
+			return;
+		}
+
+		username.splice(username.indexOf(socket.username) , 1);
+		updateUsernames();
 	});
 });
